b7155e82e0fca520d6d5d765800dedea4fe32502
6ag <admin@6ag.cn>
2016-02-19 11:26:17 +0800

    加载数据

---
+++ BaoKanIOS/BaoKanIOS/Classes/Model/JFArticleContentModel.swift
@@ -0,0 +1,13 @@
+//
+//  JFArticleContentModel.swift
+//  BaoKanIOS
+//
+//  Created by jianfeng on 16/2/19.
+//  Copyright © 2016年 六阿哥. All rights reserved.
+//
+
+import UIKit
+
+class JFArticleContentModel: NSObject {
+
+}
